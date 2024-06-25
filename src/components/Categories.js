c2da913c7caa9134eb17ec81b5a5a33a4a61cd25
CarolineB13 <158301445+CarolineB13@users.noreply.github.com>
2024-06-25 11:10:21 +0200

    P3C3-solution

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,25 +1,22 @@
 import '../styles/Categories.css'
 
-function Categories ({setActiveCategory, categories, activeCategory})
- {
+function Categories({ setActiveCategory, categories, activeCategory }) {
 	return (
 		<div className='lmj-categories'>
-	
 			<select
-            value={activeCategory}
-            onChange={(e) => setActiveCategory(e.target.value)}
-            className='lmj-categories-select'
-            >
-            <option value=''>---</option>
-            {categories.map((cat) => (
-                <option key={cat} value={cat}>
-                {cat}
-                </option>
-            ))}
-            </select>
-            <button onClick={() => setActiveCategory('')}>Réinitialiser</button>    
-        </div>
-	
+				value={activeCategory}
+				onChange={(e) => setActiveCategory(e.target.value)}
+				className='lmj-categories-select'
+			>
+				<option value=''>---</option>
+				{categories.map((cat) => (
+					<option key={cat} value={cat}>
+						{cat}
+					</option>
+				))}
+			</select>
+			<button onClick={() => setActiveCategory('')}>Réinitialiser</button>
+		</div>
 	)
 }
 
